43046cf12dab0ae770dba4b9bb2f5190eb32607e
marina <mariina.karpenko@gmail.com>
2023-06-01 13:13:50 +0200

    chenge

---
+++ project_12/script.js
@@ -0,0 +1,63 @@
+// alert("hello") // allert - это уведомление в браузере
+// console.log("hello") // для поиска ошибок, для отображения информ
+
+// const name = prompt("Как тебя зовут?") //функция для ввода информ
+// console.log(name)
+
+// const userAge = 40
+// const userLogin = "Starte Institute"
+// console.log(userAge) // значение переменной userAge
+// console.log("userAge") // строка "userAge"
+
+// const productName = "Milk"
+// console.log(productName)
+
+// const userName = "Maryna"
+// console.log(userName)
+
+// const roductPrice = 10000
+// console.log(roductPrice)
+
+const productName = "Milk"
+const userName = "Maryna"
+const roductPrice = 10000
+console.log(productName, userName, roductPrice)
+
+/* 
+    1. строка - "Arsen", "Hello", "My title", "2023", "500"
+    2. число - 100, 500, 2023, 700, 1000, 10.5, 3.14
+*/
+
+/*
+    Задачи
+    1. Создать переменные для хранения назв прод, имени польз, цены прод
+    2. Отобр каждую в консоли
+*/
+
+/*
+    Переменная - это контейнер для данных
+    Для создания переменной есть два ключ слова - let, const
+
+    Правала наименования переменных:
+        1. назв должно быть написано на латинеце - name, age, text, title
+        2. в переменной могут использоваться цифры, только не в начале - 
+            name1 - правильно
+            1name - неправильно
+        3. переменная дрлжна быть осмысленной
+            Если переменная хранит имя - name
+            Усли возраст - age
+        4. cameCase - первая буква первого слова начинается с маленькой буквы, остальные слова с большой
+            переменная хранит цену продукта - productPrice
+            переменная хранит цену продукта со скидкой - productPriceWithSale
+
+            snake_case - каждое последуещее слово написано через нижний слеш "_"
+*/
+
+// 1. Ввксти в prompt имя пользователя
+// 2. Введенное имя отобразить в consol
+
+// это однострочный комент
+/* 
+многострочный комент
+    ctrl + / - Это закоментировать строку
+ */
