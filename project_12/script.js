5b9fa5d2abd73a514e27d772095c12b740856bd7
marina <mariina.karpenko@gmail.com>
2023-06-05 15:08:36 +0200

    new

--- project_12/script.js
+++ project_12/script.js
@@ -18,10 +18,65 @@
 // const roductPrice = 10000
 // console.log(roductPrice)
 
-const productName = "Milk"
-const userName = "Maryna"
-const roductPrice = 10000
-console.log(productName, userName, roductPrice)
+// const productName = "Milk"
+// const userName = "Maryna"
+// const roductPrice = 10000
+// console.log(productName, userName, roductPrice)
+
+// let price = 2000
+// const price1 = 3000
+
+// price = 5000
+// price1 = 7000
+// console.log(price)
+// console.log(price1)
+
+// Задача. Запросить 2 числа у пользователя и запросить их в консоле отоьразить их сумму
+// const number = +prompt("Введите первое число") // запросили 1 часло и сохранили в переменную number
+// const number1 = +prompt("Введите второе число") // запросили 2 часло и сохранили в переменную number1
+// // console.log(number + number1) //отображаем пользователю сумму 2 чисел
+// const sum = number + number1
+// console.log(sum)
+/* конкатинация - склеивание строк
+promt - всегда возвращает строку, чтобы получить число нужно поставить "+" перед promt() */
+
+// Задача. Запросить у пользователя 1 часло. А в консоле вывести квадрат этого числа.
+// const number = +prompt("Введите число")
+// const square = number * number // квадрат числа
+// console.log(square)
+// console.log(number ** 2) //number ** 3, number ** 4
+// console.log(Math.pow(number, 2)) // Math.pow(число, степень)
+
+// Задача. Запросить у пользователя имя, в консоле вывести. "Привет", "введенное имя"
+// const name = prompt("Введите ваше имя")
+// console.log("Hello, " + name) // конкатинация / concatenation
+// console.log(`Hello, ${name}`) // интерпаляция / interpolation 
+
+// Задача. Запросить у польз имя и возраст. Вывести - "Вам введенный возраст"
+// const name = prompt("Введите ваше имя")
+// const age = +prompt("Введите ваш возраст")
+// console.log("Hello, " + name + ". " + "Вам " + age + " лет")
+// console.log(`Hello, ${name}. Вам ${age} лет`)
+
+// Задача. Есть 3 пер. r, g, b. В консоль вывести  строку
+const r = 15
+const g = 51
+const b = 120
+console.log(`rgb(${r}, ${g}, ${b})`)
+
+/* Когда мы знаем, что значение переменной будет меняться, то используем знамение let
+    В остальных случаях используем const - константа - постоянная переменная
+*/
+
+/* = это оператор писваивания, присваиваем значение пересенной, храним в переменной значение */
+
+/*
++ - сумма
+- - разница
+* - произведение
+/ - дедение
+** - возведение в степень
+*/
 
 /* 
     1. строка - "Arsen", "Hello", "My title", "2023", "500"
