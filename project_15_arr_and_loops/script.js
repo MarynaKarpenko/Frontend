d3ee544820d47be16f3f33505e46a37505ec9db4
marina <mariina.karpenko@gmail.com>
2023-06-14 23:54:51 +0200

    chenge code

--- project_15_arr_and_loops/script.js
+++ project_15_arr_and_loops/script.js
@@ -1,47 +1,5 @@
 // Масcивы и циклы
-function func_name() {
-    const names = ["Alexander", "Yulia", "Arsen"]
 
-    // В консоле отобр имя Юлия из масива names
-    console.log(names[1])
-}
-func_name()
-
-// // Создать масив в кот будут след часла - 10, 15, 0, 100, 21
-// // Отобр в консоли число 0
-// const numbers = [10, 15, 0, 100, 21]
-// console.log(numbers[2])
-
-// const arr = ["Hello", 100, true, ["Hello", "Hello World"]] // в массве могут храниться разные типы данных
-
-// const countries = ["Germany", "France", "UK"]
-// console.log(countries.length) // свойство length - длина массива или количество элиментов в нем
-
-// countries.push("Spain") // метод push - позволяет добавить значение в конец массива
-// console.log(countries.length) // 4 - длина массива увеличилась
-
-// /*
-//     Для обращения к посл эл массива используем след запись length-1
-// */
-// countries[countries.length-1]
-
-// Задача. 
-// Есть массив. Нужно попросить польз ввести имя, в введенное имя добавить в массив names, используя метод push
-// Изм код так чтобы у польз запращивалось имя дважды и оба доьавить в массив name
-// const names = ["Alexander", "Yulia", "Arsen"]
-// const name = prompt("Введите Ваше имя")
-// const name2 = prompt("Введите Ваше имя")
-// names.push(name, name2)
-// console.log(names)
-
-
-// const users = [
-//     {
-//         name: "Arsen",
-//         age: 25,
-//         hobbies: ["Football", "Design", "Box"]
-//     }
-// ]
 
 /* 
     массив - коллекция данных, нумерованный список. 
@@ -56,21 +14,86 @@ func_name()
     4. массив (объект)
 */
 
-// const fruit = ['Apple', 'Orange', 'Banana'];
-// console.log(fruit[0]); // Output: Apple
-// fruit[1] = 'Mango';
-// console.log(fruit[1]);
 
-// Nested Arrays
+// Задача 1. В консоле отобр имя Юлия из масива names
+
+function func_name1() {
+    const names = ["Alexander", "Yulia", "Arsen"]
+    console.log(names[1])
+}
+func_name1()
+
+
+// Задача 2. Создать массив в котором будут следующие числа - 10, 15, 0, 100, 21;
+// Отобр в консоли число 0;
+
+function func_name2() {
+    const numbers = [10, 15, 0, 100, 21]
+    console.log(numbers[2])
+}
+func_name2()
+
+
+function func_name() {
+    const arr = ["Hello", 100, true, ["Hello", "Hello World"]] // в массве могут храниться разные типы данных
+
+    const countries = ["Germany", "France", "UK"]
+    console.log(countries.length)           // свойство length - длина массива или количество элиментов в нем
+    
+    countries.push("Spain")                 // метод push - позволяет добавить значение в конец массива
+    console.log(countries.length)           // 4 - длина массива увеличилась
+    
+    
+    /*
+        Для обращения к посл эл массива используем след запись length-1
+    */
+    
+    countries[countries.length-1]
+}
+func_name()
+
+
+// Задача 3. Есть массив. Нужно попросить польз ввести имя, в введенное имя добавить в массив names, используя метод push
+// Изм код так чтобы у польз запрашивалось имя дважды и оба добавить в массив name
+
+function func_name3() {
+    const names = ["Alexander", "Yulia", "Arsen"]
+
+    const name = prompt("Введите Ваше имя")
+    const name2 = prompt("Введите Ваше имя")
+
+    names.push(name, name2)
+
+    console.log(names)
+}
+func_name3()
+
+
+    const users = [
+        {
+            name: "Arsen",
+            age: 25,
+            hobbies: ["Football", "Design", "Box"]
+        }
+    ]
+
+
+const fruit = ['Apple', 'Orange', 'Banana'];
+console.log(fruit[0]);               // Output: Apple
+fruit[1] = 'Mango';
+console.log(fruit[1]);               // Nested Arrays (вложенный массив)
+
 
 // Создаем вложенный массив
-// const food = [
-//     ['Apple', 'Orange', 'Banana'],
-//     ['Strawberry', 'Blueberry', 'Raspberry'],
-//     ['Potato', 'Carrot', 'Broccoli'],
-//   ];
-// console.log(food[1][2]); // 2 строка, 3 элемент // Output: Raspberry
-// console.log(food[0][1]); // Output: Orange
+
+const food = [
+    ['Apple', 'Orange', 'Banana'],
+    ['Strawberry', 'Blueberry', 'Raspberry'],
+    ['Potato', 'Carrot', 'Broccoli'],
+  ];
+console.log(food[1][2]);            // 2 строка, 3 элемент // Output: Raspberry
+console.log(food[0][1]);            // Output: Orange
+
 
 // .length() - длина - возвращает определенное количество строк в массиве
 
@@ -80,17 +103,19 @@ func_name()
 
 
 
-
 // Циклы
+
 
 /*
     Цикл - это повторяющиеся действия
 */
 
+
 // В консоле отобр 10 раз "Hello"
-// for(let i = 0; i < 10; i++) {
-//     console.log("Hello")
-// }
+
+for(let i = 0; i < 10; i++) {
+    console.log("Hello")
+}
 
 
 /*
@@ -107,32 +132,53 @@ func_name()
 11. i = 10, 10 < 10 - FALSE. Выход из цикла
 */
 
-// Задача.
+
+// Задача 4.
 // В консоле отобр текст "Hello World" 50 раз
 
-// for(let i =0; i < 50; i++) {
-//     console.log("Hello World")
-// }
-
-// Task. В console отобр числа от 1 до 100
-// а. не вкл 100
-// for(let i =0; i < 100; i++) {
-//     console.log(i)
-// }
-// б. вкл 100
-// for(let i =1; i < 100; i++) {
-//     console.log(i + 1)
-// }
-// for(let i =1; i <= 100; i++) {
-//     console.log(i) // второй вариант
-// }
+function func_name4() {
+    for(let i =0; i < 50; i++) {
+        console.log("Hello World")
+    }
+}
+func_name4()
 
 
-// Task. В console отобр все четные числа от 1 до 100, 
-// for(let i =0; i < 100; i++) { // Если при делении на 2 в ост 0
-//     if(i % 2 == 0)
-//     console.log(i)
-// }
+// Задача 5. В console отобр числа от 1 до 100
+
+// а). не вкл 100
+
+function func_name5a() {
+    for(let i =0; i < 100; i++) {
+        console.log(i)
+    }
+}
+func_name5a()
+
+// б). вкл 100
+
+function func_name5b() {
+    for(let i =1; i < 100; i++) {
+    console.log(i + 1)
+}
+
+for(let i =1; i <= 100; i++) {
+    console.log(i)             // второй вариант
+}
+}
+func_name5b()
+
+
+// Задача 6. В console отобр все четные числа от 1 до 100, 
+
+function func_name6() {
+    for(let i =0; i < 100; i++) { // Если при делении на 2 в ост 0
+        if(i % 2 == 0)
+        console.log(i)
+    }
+}
+func_name6()
+
 
 /*
     1. i = 0, 0 % 2 ==
@@ -140,6 +186,7 @@ func_name()
     3. i = 2, 2 % 2 = 0, - четное. 2/2=1, значит в остатке 0. 2 делится на 2 без остатка
     4. i = 3, 3 % 2 = 1, - нечетное. 3/3=1, значит в остатке 1. 3 неделится на 2 без остатка, остаток 1
 */
+
 
 /*
 
@@ -153,6 +200,7 @@ func_name()
     Нечетное число при делении на 2 в ост дает 1
 */
 
+
 /*
     Цикл for
 
@@ -160,17 +208,17 @@ func_name()
         тело цикла
     }
 
-    1. инициализация - создаем счетчик. let i - iteration/итерация - повторение
-    2. условие - мемент до которого нащ счетчик должен выполняться
-    3. шаг - после каждого действия, увеличивается шаг счетчика до момента завершения
-    i++ - увеличение пересенной на 1
+        1. инициализация - создаем счетчик. let i - iteration/итерация - повторение
+        2. условие - мемент до которого нащ счетчик должен выполняться
+        3. шаг - после каждого действия, увеличивается шаг счетчика до момента завершения
+            i++ - увеличение пересенной на 1
+
 
     Алгоритм работы цикла:
-    1.  Выполняется начало - let i = 0
-    2. Выполняется условие - i < 10
-        Если условие true - выполняется действие в теле цикла - console.log("Hello")
-
-    3. Выаолняется шаг - увиличение переменной i на 1. Опять переходим к пункту 2
+        1.  Выполняется начало - let i = 0
+        2. Выполняется условие - i < 10
+            Если условие true - выполняется действие в теле цикла - console.log("Hello")
+        3. Выаолняется шаг - увиличение переменной i на 1. Опять переходим к пункту 2
         Если false - выход из цикла
 
             let i = 0
@@ -183,35 +231,54 @@ func_name()
 
 */
 
-// Есть массив names. В console Отобр каждое имя из массива
-// const names = ["Arsen", "Sergey", "Bogdan", "Elena"] // длина массива 4
-// for(let i = 0; i < names.length; i++) {
-//     console.log(names[i])
-// }
 
-// Task.  Есть массив чисел numbers. Нужно вывести квадрат каждого числа чассива 
-// const numbers = [10, 20, 5, 11, 6, 4]
-// for(let i = 0; i < numbers.length; i++) {
-//     console.log(numbers[i] * numbers[i]) 
-//     console.log(Math.pow(numbers[i], 2)) // второй способ возведения в степень
-// }
-// console.log(numbers[i]) - каждый ел массива 
+// Задача 7. Есть массив names. В console Отобр каждое имя из массива
 
-// Task. Ксть массив строк. Нужно вывести в консоле каждую строку в верхнем регистре
-// const countries = ["Germany", "France", "Spain", "Chech"]
-// for(let i = 0; i < countries.length; i++) {
-//     // console.log(countries[0].toUpperCase())
-//     // console.log(countries[1].toUpperCase())
-//     // console.log(countries[2].toUpperCase())
-//     // console.log(countries[3].toUpperCase())
-//     console.log(countries[i].toUpperCase())
-// }
+function func_name7() {}
+    const names = ["Arsen", "Sergey", "Bogdan", "Elena"] // длина массива 4
+    for(let i = 0; i < names.length; i++) {
+        console.log(names[i])
+}
+func_name7()
+
+
+// Задача 8.  Есть массив чисел numbers. Нужно вывести квадрат каждого числа массива 
+
+function func_name8() {
+    const numbers = [10, 20, 5, 11, 6, 4]
+for(let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] * numbers[i]) 
+
+    console.log(Math.pow(numbers[i], 2)) // второй способ возведения в степень
+}
+console.log(numbers)                  // каждый ел массива 
+}
+func_name8()
+
+
+// Задача 9. Ксть массив строк. Нужно вывести в консоле каждую строку в верхнем регистре
+
+function func_name9() {
+    const countries = ["Germany", "France", "Spain", "Chech"]
+for(let i = 0; i < countries.length; i++) {
+
+    // console.log(countries[0].toUpperCase())
+    // console.log(countries[1].toUpperCase())
+    // console.log(countries[2].toUpperCase())
+    // console.log(countries[3].toUpperCase())
+
+    console.log(countries[i].toUpperCase())
+}
+}
+func_name9()
+
 
 // for(let i = 0; i < countries.length; i++) {
 //     const upperCaseCountry = countries[i].toUpperCase()
 //     console.log(upperCaseCountry)
 //     countries.push(upperCaseCountry) // если хотим заполнить другой массив странами в верхнем регистре
 // }
+
 
 /*
 string.toUpperCase()
--- project_23_Creating_DOM_Elements/script.js
+++ project_23_Creating_DOM_Elements/script.js
@@ -3,6 +3,7 @@
 
 // 1. cresteElement() - метод создания элемента
 
+
 /*
 два подхода:
 императивный и декларативный
@@ -14,34 +15,48 @@
         4. Добавляем созданный тег в найденный в п1 тег. appendChild()
 */ 
 
-// const header = document.querySelector("header") // находим элемент, куда будем добавлять созданный тег
-// const title = document.createElement("h1") // создаю элемент
-// title.innerText = "My title"
-// header.appendChild(title) // добавляем созданный элемент (title - h1) в header
+
+const header = document.querySelector("header")   // находим элемент, куда будем добавлять созданный тег
+const title = document.createElement("h1")        // создаю элемент
+title.innerText = "My title"
+header.appendChild(title)                         // добавляем созданный элемент (title - h1) в header
+
 
 // 2. innertHTML - свойство, котокое позволяет получить или назначить html содержимое тега
-// const main = document.querySelector("main")
-// console.log(main.innerHTML)
 
-// Задача. в header добавать h1 с текстом myTitle
+const main = document.querySelector("main")
+console.log(main.innerHTML)
+
+
+// Задача 1. в header добавать h1 с текстом myTitle
+
 // const header = document.querySelector("header")
 // header.innerHTML = "<h1> My Title </h1>"
+
 
 /*
     метод querySelector возвращает null, если неправильно написали селектор
         или по заданному пути нет тега в html
 */
 
-// Задача. Содать цикл, кот позволяет добавить 10 тегов p с текстом Hello внутрь section
-// const section = document.querySelector("section");
-//     for (let i = 0; i < 10; i++) {
-//         section.innerHTML += "<p>Hello</p>";
-//     }
 
-// const section = document.querySelector("main section");
-// for (let i = 0; i < 10; i++) {
-//     section.innerHTML += "<p>Hello World</p>"; // + для добавления к имеющемуся тегу несколько тегов
-// }
+// Задача 2. Содать цикл, кот позволяет добавить 10 тегов p с текстом Hello внутрь section
+
+function func_name2() {
+    const section = document.querySelector("section")
+
+// section.innerHTML = "<p> Hello World </p>"
+// section.innerHTML += "<p> Hello World 1</p>"
+// section.innerHTML += "<p> Hello World 1</p>"
+// section.innerHTML += "<p> Hello World 1</p>"
+// section.innerHTML += "<p> Hello World 1</p>"
+// section.innerHTML += "<p> Hello World 1</p>"
+
+    for(let i = 0; i < 10; i++) {
+            section.innerHTML += "<p> Hello </p>"
+        }
+}
+func_name2()
 
 
 /*  
@@ -51,20 +66,26 @@
     2. Свойство innerHTML позволяет считать содержимое элемента в виде HTML-строки или установить новый HTML.
 */
 
-// Задача. Есть массив имен names. 
+
+// Задача 3. Есть массив имен names. 
 // Для каждого имени создать тег p и добавить его в section
 
-// const names = ["Arsen", "Yuliya", "Lana", "Maria"]
+function func_name3() {
+    const names = ["Arsen", "Yuliya", "Lana", "Maria"]
 
-// const section = document.querySelector("section")
+    const section = document.querySelector("section")
 
-// for (let i = 0; i < names.length; i++) {
-//     section.innerHTML += `<p class = "text"> ${names[i]} </p>`
-// }
+    for (let i = 0; i < names.length; i++) {
+        section.innerHTML += `<p class = "text"> ${names[i]} </p>`
+    }
+}
+func_name3()
 
 
 
-// Задача. Есть массив объектов prodcuts. Для каждого продукта создать разметку
+
+// Задача 4. Есть массив объектов prodcuts. Для каждого продукта создать разметку
+
 /*
     <div class="product">
         <h5>Назввание продукта</h5>
@@ -72,90 +93,43 @@
     </div>
 */
     
-
-// const products = [
-//     {
-//         name: "Iphone 13",
-//         price: 10000
-//     },
-//     {
-//         name: "Iphone 14",
-//         price: 20000
-//     },
-//     {
-//         name: "Iphone 15",
-//         price: 25000
-//     },
-// ]
-// const section = document.querySelector("section")
-// let sum = 0
-// for (let i = 0; i < products.length; i++) {
-//     sum = sum + products[i].price  
-// }
-
-// for (let i = 0; i < products.length; i++) {
-//     section.innerHTML += ` <div class="product">
-//                                 <h5> ${products[i].name} </h5>
-//                                 <p> ${products[i].price} </p>
-//                            </div> `
+function func_name4() {
+    const products = [
+        {
+            name: "Iphone 13",
+            price: 10000
+        },
+        {
+            name: "Iphone 14",
+            price: 20000
+        },
+        {
+            name: "Iphone 15",
+            price: 25000
+        },
+    ]
+    const section = document.querySelector("section")
+    let sum = 0
+    for (let i = 0; i < products.length; i++) {
+        sum = sum + products[i].price  
+    }
     
-// }
-// section.innerHTML += `<h1 class="summa">Сумма всех продуктов составляет ${sum}</h1>`
+    for (let i = 0; i < products.length; i++) {
+        section.innerHTML += ` <div class="product">
+                                    <h5> ${products[i].name} </h5>
+                                    <p> ${products[i].price} </p>
+                               </div> `
+        
+    }
+    section.innerHTML += `<h1 class="summa">Сумма всех продуктов составляет ${sum}</h1>`
+}
+func_name4()
 
 // Добавить в разметку <h1></h1>, в котором должна быть общая стоимость всех продуктов
 // 1. с помощью цикла for посчитать сумму всех продуктов
 // 2. добавить h1 в конец section, в котором указать цену
 
 
-// const sumTitle = document.createElement("h1")
-// sumTitle.innerText =  `<h1>Сумма продуктов составляет ${sum}</h1>`
-// section.appendChild(sumTitle)
-
-
-// Задача. Есть массив products. 
-// 1. Для каждого объекта в массиве products создать разметку
-// 2. Отобразить только те продукты, цена которых больше 10000
-// 3. Отобразить только те продукты, категория которых - phone
-
-const products = [
-    {
-        name: "Iphone 13",
-        category: "phone",
-        price: 15000
-    },
-    {
-        name: "Iphone 14",
-        category: "phone",
-        price: 20000
-    },
-    {
-        name: "Acer",
-        category: "laptop",
-        price: 40000
-    },
-    {
-        name: "Asus",
-        category: "laptop",
-        price: 35000
-    },
-    {
-        name: "Samsung",
-        category: "TV",
-        price: 27000
-    },
-    {
-        name: "LG",
-        category: "TV",
-        price: 7000
-    },
-]
-
-/*
-    <div class="product">
-        <h3> {имя продукта} </h3>
-        <p> {категория продукта} </p>
-        <p> {цена продукта} </p>
-        <button>Купить</button>
-    </div>
-*/
-  
+    // const sumTitle = document.createElement("h1")
+    // sumTitle.innerText =  `<h1>Сумма продуктов составляет ${sum}</h1>`
+    // section.appendChild(sumTitle)
