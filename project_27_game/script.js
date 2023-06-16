830792e094139d0cbb7d3e1e4b2abec302c60f69
marina <mariina.karpenko@gmail.com>
2023-06-16 13:06:33 +0200

    change code

--- project_27_game/script.js
+++ project_27_game/script.js
@@ -6,20 +6,24 @@ function start_game() {
 function finish_game() {
     // console.log('finish game works');
     object.classList.remove('start')
-    // alert(`You lose. Score ${score}`)
+    alert(`You lose. Score ${score}`)
 }
 
 function hit() {
     // console.log("hit works");
     score++;
-    document.title = 'Score ${score}';
+    document.title = `Score ${score}`;
 
     object.classList.remove('start');
     void object.offsetWidth; // магия. переключение класса. возврат в исходное состояние
     object.classList.add('start');
 
     let random_offset = Math.random() * 340;
-    object.style.left = '${random_offset}px'; 
+    object.style.left = `${random_offset}px`; 
+
+    change_object_background();
+    hit_sound.currentTime = 0;
+    hit_sound.play();
 }
 
 function change_object_background(){
@@ -38,8 +42,13 @@ function miss(event) {
         }
 
         document.title = `Score ${score}`;
+        miss_sound.currentTime = 0;
+        miss_sound.play();
     }
 }
 
 let score = 0;
 let object = document.querySelector('#object');
+
+const hit_sound = new Audio('sounds/hit.wav');
+const miss_sound = new Audio('sounds/miss.wav');
